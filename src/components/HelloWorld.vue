2c73f3316fcecd2516063a1e0160c42f8faee873
Eric Dao <eric_dao@hotmail.com>
2022-04-27 21:02:57 -0700

    vue create project, firebase, add to firebase

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <form @submit.prevent="onSubmit">
+      <div>
+        <label>Name</label>
+        <input v-model="user.name" required />
+      </div>
+      <div>
+        <label>Email</label>
+        <input v-model="user.email" type="email" required />
+      </div>
+      <button type="submit">Create User</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { db } from "../firebaseDb";
+import { collection, addDoc } from "firebase/firestore";
+export default {
+  data() {
+    return {
+      user: {},
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      addDoc(collection(db, "test"), this.user)
+        .then(() => {
+          alert("User Created!");
+          this.user.name = "";
+          this.user.email = "";
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      // db.collection("test")
+      //   .add(this.user)
+      //   .then(() => {
+      //     alert("User successfully created!");
+      //     this.user.name = "";
+      //     this.user.email = "";
+      //   })
+      //   .catch((error) => {
+      //     console.log(error);
+      //   });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
