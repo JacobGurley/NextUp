71a31f81b2eeecdbf7b7a79349c0b1b7ab372428
Eric Dao <ericdao@Erics-MacBook-Air.local>
2022-09-19 14:28:00 -0700

    add background image

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Homepage</h1>
+  <div
+    class="homepage"
+    :style="{
+      'background-image':
+        'url(https://image.roku.com/blog/wp-content/uploads/2020/07/Live_Sports_graphic_1200x628.jpg)',
+      'background-repeat': 'no-repeat',
+      'background-size': 'cover',
+    }"
+  >
+    <h1>Homepage</h1>
+  </div>
 </template>
+<style scoped>
+h1 {
+  color: white;
+}
+.homepage {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
