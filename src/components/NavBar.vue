7d9c5cfc85272e6d9ef0ef613c70e49358025ff8
Eric Dao <ericdao@Erics-MacBook-Air.local>
2022-10-07 16:28:35 -0700

    send team info to database, add logout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -25,11 +25,12 @@
             </router-link>
           </li>
           <li class="nav-item">
-            <a
+            <router-link
+              to="createteam"
               class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
             >
               <span class="ml-2">Create Team</span>
-            </a>
+            </router-link>
           </li>
           <li class="nav-item">
             <a
@@ -43,8 +44,41 @@
               ><span class="ml-2">Contact Us</span>
             </a>
           </li>
+          <li class="nav-item">
+            <button
+              @click="handleSignout"
+              class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+            >
+              Logout
+            </button>
+          </li>
         </ul>
       </div>
     </div>
   </nav>
 </template>
+<script setup>
+import { onMounted, ref } from "vue";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import router from "../router";
+
+const isLoggedIn = ref(false);
+let auth;
+onMounted(() => {
+  console.log("in onmounted");
+  auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    console.log(isLoggedIn);
+    if (user) {
+      isLoggedIn.value = true;
+    } else {
+      isLoggedIn.value = false;
+    }
+  });
+});
+const handleSignout = () => {
+  signOut(auth).then(() => {
+    router.push("/");
+  });
+};
+</script>
