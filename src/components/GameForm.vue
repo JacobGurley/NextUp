700a3b9c8183fa3512707dddb065f1e5791e0c39
Eric Dao <ericdao@Erics-Air.lan>
2022-10-07 21:00:23 -0700

    form validation

--- src/components/GameForm.vue
+++ src/components/GameForm.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="w-full max-w-xs">
-    <form class="bg-stone-600 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <form
+      @submit.prevent="onSubmit"
+      class="bg-stone-600 shadow-md rounded px-8 pt-6 pb-8 mb-4"
+    >
       <h1 class="text-white mb-5 font-bold text-xl">Create Game</h1>
       <div class="mb-4">
         <label class="block text-white text-sm font-bold mb-2" for="roomname">
@@ -51,13 +54,26 @@
       </div> -->
       <div>
         <button
-          @click="createGame"
+          @click="validate"
           class="bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full"
         >
           Create Game
         </button>
       </div>
     </form>
+    <div v-if="validationErrors.length">
+      <button @click="resetError()" class="delete"></button>
+      <div class="content">
+        Please resolve the following error(s) before proceeding.
+        <ul style="margin-top: 0.3em; margin-left: 1em">
+          <li
+            v-for="(error, index) in validationErrors"
+            :key="`error-${index}`"
+            v-html="error"
+          />
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -70,6 +86,7 @@ export default {
       sport: null,
       numPlayers: null,
       location: null,
+      validationErrors: [],
     };
   },
   methods: {
@@ -81,6 +98,45 @@ export default {
         numberofplayers: this.numPlayers,
         location: this.location,
       });
+      this.roomName = null;
+      this.sport = null;
+      this.numPlayers = null;
+      this.location = null;
+    },
+    resetError() {
+      this.validationErrors = [];
+    },
+    validate() {
+      // Clear the errors before we validate again
+      this.resetError();
+      // email validation
+      if (!this.roomName) {
+        this.validationErrors.push(
+          "<strong>Room Name</strong> cannot be empty."
+        );
+      }
+      if (!this.sport) {
+        this.validationErrors.push("<strong>Sport</strong> cannot be empty.");
+      }
+      if (!this.numPlayers) {
+        this.validationErrors.push(
+          "<strong>Number of Players</strong> cannot be empty."
+        );
+      }
+      if (isNaN(this.numPlayers)) {
+        this.validationErrors.push(
+          "<strong>Number of Players</strong> must be a number."
+        );
+      }
+      if (!this.location) {
+        this.validationErrors.push(
+          "<strong>Location</strong> cannot be empty."
+        );
+      }
+      // when valid then sign in
+      if (this.validationErrors.length <= 0) {
+        this.createGame();
+      }
     },
   },
 };
