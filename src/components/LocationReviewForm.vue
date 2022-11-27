29ae4cab50b48f961e3896de3b620172e9c18cc6
edgar94r <72772373+edgar94r@users.noreply.github.com>
2022-11-26 20:02:05 -0800

    location review code

---
+++ src/components/LocationReviewForm.vue
@@ -0,0 +1,61 @@
+<!--This creates a form that allows users to type. It also links to firebase.-->
+<template>
+    <div class="w-full max-w-xs">
+      <form class="bg-neutral-700 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 class="text-white mb-5 font-bold text-xl">Location Review</h1>
+        <div class="mb-4">
+          <label class="block text-white text-sm font-bold mb-2" for="locationName">
+            Location Name
+          </label>
+          <input
+            v-model="locationName"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="locationname"
+            type="text"
+          />
+        </div>
+        <div class="mb-6">
+          <label class="block text-white text-sm font-bold mb-2" for="comments">
+            Comments
+          </label>
+          <input
+            v-model="comments"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="comments"
+            type="text"
+          />
+        </div>
+        <div class="star-rating">
+            <star-rating :rating="rating" :star-style="starStyle"></star-rating>
+        </div>
+      </form>
+    </div>
+  </template>
+
+  <script>
+import StarRating from 'vue-dynamic-star-rating'
+  export default{
+   components: {
+  StarRating
+},
+data: function(fullStarColor) {
+    return {
+        rating: 4.7,
+        starStyle: {
+            fullStarColor: '#ed8a19',
+            emptyStarColor: '#737373',
+            starWidth: 30,
+            starHeight: 30
+        }
+    }
+}
+}
+
+</script>
+
+<style>
+.star-rating{
+    color: gold;
+}
+</style>
+  
---
+++ src/components/LocationReviewPage.vue
@@ -0,0 +1,48 @@
+<template>
+    <Nav />
+    <div
+      class="location-review"
+      :style="{
+        'background-image':
+          'url(https://image.roku.com/blog/wp-content/uploads/2020/07/Live_Sports_graphic_1200x628.jpg)',
+        'background-repeat': 'no-repeat',
+        'background-size': 'cover',
+      }"
+    >
+      <LocationReviewForm />
+    </div>
+  </template>
+
+  <script scoped>
+  import LocationReviewForm from "./LocationReviewForm.vue";
+  import Nav from "./NavBar.vue";
+  
+  export default {
+    components: {
+      LocationReviewForm,
+      Nav,
+    },
+  };
+  </script>
+
+  <style scoped>
+  .location-review {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+  }
+  .background-image {
+    background-position: center bottom;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  </style>
+  
